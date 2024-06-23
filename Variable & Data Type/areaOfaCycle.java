96042d7afd03622f0d75ae828a2c848c75bc4cbb
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-06-23 22:10:21 +0600

    area of a cycle

---
+++ Variable & Data Type/areaOfaCycle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class areaOfaCycle {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        float radious = sc.nextFloat();
+        float cycleArea = 3.1416f * radious * radious;
+        System.out.println(cycleArea);
+    }
+}
