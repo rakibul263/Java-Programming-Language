154b318bca85ffcb29d57013b5b4f7079aefc57b
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-06-23 13:10:56 +0600

    primitive data type

---
+++ Variable & Data Type/primitive_data_type.java
@@ -0,0 +1,30 @@
+public class primitive_data_type {
+    public static void main(String args[]) {
+        byte a = 8;
+        System.out.println(a);
+
+        Short b = 10;
+        System.out.println(b);
+
+        char c = 'a';
+        System.out.println(c);
+
+        boolean d = false;
+        System.out.println(d);
+
+        int e = 45;
+        System.out.println(e);
+
+        long f = 475298347;
+        System.out.println(f);
+
+        /*
+         * float price = 1.5;
+         * System.out.println(price);
+         */
+
+        double h = 4.45982347;
+        System.out.println(h);
+
+    }
+}
