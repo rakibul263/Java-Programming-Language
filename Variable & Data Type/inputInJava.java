b6ca021fdf48390eb145eaefab6e63e228479c16
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-06-23 15:13:07 +0600

    input in java

---
+++ Variable & Data Type/inputInJava.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class inputInJava {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        /*
+         * //input word
+         * String input = sc.next();
+         * System.out.println(input);
+         */
+
+        /*
+         * // input sentace
+         * String input = sc.nextLine();
+         * System.out.println(input);
+         */
+
+        /*
+         * // input integer
+         * int input = sc.nextInt();
+         * System.out.println(input);
+         */
+
+        // input float
+        float input = sc.nextFloat();
+        System.out.println(input);
+
+    }
+}
