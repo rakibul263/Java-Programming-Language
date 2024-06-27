848c5ebdc1662b7e82670ced0054ec5f035fe0f7
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-06-27 09:11:46 +0600

    Logical AND

---
+++ Operator/Logical Operator/Logical_AND.java
@@ -0,0 +1,10 @@
+public class Logical_AND {
+    public static void main(String args[]) {
+        // logical and
+        System.out.println("Logical AND: ");
+        System.out.println("one statement True and One Statement False : " + ((5 > 2) && (3 < 10)));
+        System.out.println("one statement True and One Statement False : " + ((5 > 2) && (5 < 2)));
+        System.out.println("one statement True and One Statement False : " + ((5 < 2) && (5 > 2)));
+        System.out.println("one statement True and One Statement False : " + ((5 < 2) && (5 < 2)));
+    }
+}
