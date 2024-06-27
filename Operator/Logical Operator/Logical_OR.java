9a52e097a161961f36acf2371019b609d2b4b5a5
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-06-27 09:24:45 +0600

    bug fixed

--- Operator/Logical Operator/Logical_OR.java
+++ Operator/Logical Operator/Logical_OR.java
@@ -1,6 +1,6 @@
 public class Logical_OR {
     public static void main(String args[]) {
-        // logical and
+        // logical OR
         System.out.println("Logical AND: ");
         System.out.println("one statement True and One Statement True : " + ((5 > 2) || (3 < 10)));
         System.out.println("one statement True and One Statement False : " + ((5 > 2) || (5 < 2)));
