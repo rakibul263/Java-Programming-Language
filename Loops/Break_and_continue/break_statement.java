2d1860c52c0916b963f9d9431a3933a6392dde88
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-06-29 11:50:10 +0600

    Flow Control

---
+++ Loops/Break_and_continue/break_statement.java
@@ -0,0 +1,10 @@
+public class break_statement {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 3) {
+                break;
+            }
+            System.out.println("Hello World");
+        }
+    }
+}
