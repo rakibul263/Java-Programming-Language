2b05bb47297f8a8d16ac33c9cce446903619bbbf
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-06-29 09:15:03 +0600

    Flow Control

---
+++ Loops/do while loop/do_while.java
@@ -0,0 +1,11 @@
+//print hello world 10 times
+
+public class do_while {
+    public static void main(String args[]) {
+        int counter = 0;
+        do {
+            System.out.println("Hello World");
+            counter++;
+        } while (counter < 10);
+    }
+}
