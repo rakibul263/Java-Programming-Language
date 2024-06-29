91ef3ce3c12df516e635b43e9a1991e2e20dcda1
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-06-29 12:02:47 +0600

    Flow Control

---
+++ Loops/While loop/print_100_times.java
@@ -0,0 +1,11 @@
+//print hello world 100 times
+
+public class print_100_times {
+    public static void main(String args[]) {
+        int counter = 0;
+        while (counter < 100) {
+            System.out.println("Hello World");
+            counter++;
+        }
+    }
+}
