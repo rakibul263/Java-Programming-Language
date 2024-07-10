bac6f7c65a6fa7b31033dd926b70ac74ac2e05a1
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-07-10 20:28:25 +0600

    string

---
+++ String/convert_letter_uppercase.java
@@ -0,0 +1,24 @@
+/* For a given String convert each the first letter of each
+word to uppercase. */
+public class convert_letter_uppercase {
+    public static String uppercase_letter(String str) {
+        StringBuilder sb = new StringBuilder("");
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ' ' && i < str.length() - 1) {
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "i am rakibul hasan";
+        System.out.println(uppercase_letter(str));
+    }
+}
