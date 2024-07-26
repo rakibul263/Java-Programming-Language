ba05e7a3ee072e33f5c9e29cc192b0d07a16e5ef
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-07-27 01:36:31 +0600

    Collection Framework

---
+++ Collection Framework/Array List/arrayList.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class arrayList {
+    public static void main(String[] args) {
+        ArrayList<String> StudentName = new ArrayList<>();
+        StudentName.add("Rakibul Hasan");
+        StudentName.add("Aysha Siddika");
+        StudentName.add("Raha Moni");
+
+        System.out.println(StudentName);
+    }
+}
---
+++ Collection Framework/Array List/list_code.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class list_code {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+
+        // remove from the list
+        list.remove(2);
+        System.out.println(list);
+
+        list.add(4); // this will add 4 end of the list...
+        System.out.println(list);
+
+        list.add(1, 100);
+        System.out.println(list);
+
+        List<Integer> newList = new ArrayList<>();
+        newList.add(129);
+        newList.add(263);
+
+        list.addAll(newList);
+        System.out.println(list);
+
+        // position getting function
+        System.out.println(list.get(1));
+
+        // full list remove
+        list.clear();
+        System.out.println(list);
+
+        System.out.println("Value increase of this specefic index :");
+        List<Integer> increaseIndex = new ArrayList<>();
+        increaseIndex.add(100);
+        increaseIndex.add(200);
+        increaseIndex.add(300);
+
+        increaseIndex.set(1, 2000);
+        System.out.println(increaseIndex);
+
+        System.out.println(increaseIndex.contains(200));
+
+    }
+}
