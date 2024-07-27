e0ad75fa7fd7657ffbd2f90f92f63394c072241b
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-07-27 11:19:15 +0600

    linked list using list

--- Collection Framework/List/Linked List/linkedList.java
+++ Collection Framework/List/Linked List/linkedList.java
@@ -1,20 +1,51 @@
 import java.util.*;
 
-// queue flow first in first out 
 public class linkedList {
     public static void main(String[] args) {
-        Queue<Integer> queue = new LinkedList<>();
-        // for element add
-        queue.offer(12);
-        queue.offer(23);
-        queue.offer(35);
+        List<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
 
-        System.out.println(queue);
+        // remove from the list
+        list.remove(2);
+        System.out.println(list);
 
-        System.out.println(queue.poll());
+        list.add(4); // this will add 4 end of the list...
+        System.out.println(list);
 
-        System.out.println(queue);
+        list.add(1, 100);
+        System.out.println(list);
 
-        System.out.println(queue.peek());
+        List<Integer> newList = new ArrayList<>();
+        newList.add(129);
+        newList.add(263);
+
+        list.addAll(newList);
+        System.out.println(list);
+
+        // position getting function
+        System.out.println(list.get(1));
+
+        // full list remove
+        list.clear();
+        System.out.println(list);
+
+        System.out.println("Value increase of this specefic index :");
+        List<Integer> increaseIndex = new ArrayList<>();
+        increaseIndex.add(100);
+        increaseIndex.add(200);
+        increaseIndex.add(300);
+
+        increaseIndex.set(1, 2000);
+        System.out.println(increaseIndex);
+
+        System.out.println(increaseIndex.contains(200));
+
+        for (Integer element : increaseIndex) {
+            System.out.println("The element is : " + element);
+        }
+
     }
 }
