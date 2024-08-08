d904bbcc925f08f0e5d887bec85ad708525f36b9
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-08-08 22:04:43 +0600

    array list problem

---
+++ Collection Framework/List/Array List/container_with_most_water_optimized.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class container_with_most_water_optimized {
+    public static int StoreWater(ArrayList<Integer> height) {
+        int maxWater = 0;
+        int lp = 0;
+        int rp = height.size() - 1;
+
+        while (lp < rp) {
+            int ht = Math.min(height.get(lp), height.get(rp));
+            int width = rp - lp;
+            int currentWater = ht * width;
+            maxWater = Math.max(maxWater, currentWater);
+
+            if (height.get(lp) < height.get(rp)) {
+                lp++;
+            } else {
+                rp--;
+            }
+        }
+        return maxWater;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(8);
+        list.add(6);
+        list.add(2);
+        list.add(5);
+        list.add(4);
+        list.add(8);
+        list.add(3);
+        list.add(7);
+
+        System.out.println(StoreWater(list));
+    }
+}
