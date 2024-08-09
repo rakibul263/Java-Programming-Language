41655c28663942164718c9f6191cac24bf8de2dc
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-08-09 11:38:21 +0600

    array list problem optimized

---
+++ Collection Framework/List/Array List/pair_sum_2_optimized.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class pair_sum_2_optimized {
+    public static boolean pairSum(ArrayList<Integer> list, int target) {
+        int n = list.size();
+        int pivot = -1;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > list.get(i + 1)) {
+                pivot = i;
+                break;
+            }
+            int lp = pivot + 1;// smallest
+            int rp = pivot; // largest
+            while (lp != rp) {
+                // case 01
+                if (list.get(lp) + list.get(rp) == target) {
+                    return true;
+                } else if (list.get(lp) + list.get(rp) < target) {
+                    // case 02
+                    // lp++;
+                    lp = (lp + 1) % n;
+                } else {
+                    // case 03
+                    // rp--;
+                    rp = (n + rp - 1) % n;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        // sorted and roted array
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(11);
+        list.add(15);
+        list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        int target = 16;
+
+        // boolean found = pairSum(list, target);
+        // if (found) {
+        // System.out.println("Found");
+        // } else {
+        // System.out.println("Not Found");
+        // }
+        System.out.println(pairSum(list, target));
+
+    }
+}
