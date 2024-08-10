f2d5d929293ef5b98b3dcd7079ab674ec0b09564
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-08-10 12:17:00 +0600

    create a add First method for node add

--- Linked LIst/LinkedList.java
+++ Linked LIst/LinkedList.java
@@ -12,9 +12,27 @@ public class LinkedList {
     public static Node head;
     public static Node tail;
 
+    public void addFirst(int data) {
+        // step-01: create a new node
+        Node newNode = new Node(data);
+
+        // base case
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        // step-02: newNode = head
+        newNode.next = head; // link
+
+        // head = newNode
+        head = newNode;
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.head = new Node(1);
-        ll.head.next = new Node(2);
+
+        ll.addFirst(1);
+        ll.addFirst(2);
     }
 }
