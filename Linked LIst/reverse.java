fa37d45468f28f7c0d5be68c1d2d1cff7e49c5f6
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-08-11 02:02:58 +0600

    reverse linkedlist

---
+++ Linked LIst/reverse.java
@@ -0,0 +1,74 @@
+public class reverse {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void rec() {// reverse
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    public void print() { // * Time complexity -> O(n) */
+        // ! base case
+        if (head == null) {
+            System.out.println("Linked List is NULL");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        reverse ll = new reverse();
+        ll.addFirst(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+
+        ll.print();
+        ll.rec();
+        ll.print();
+
+    }
+}
