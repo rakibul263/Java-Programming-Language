af1ec4b362e08dc6dd69ab6055dbcf01d2195ee6
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-08-12 12:18:19 +0600

    marge sort

---
+++ Linked LIst/MargeSort.java
@@ -0,0 +1,103 @@
+
+public class MargeSort {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public Node getMid() {
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+   private  Node marge(Node head1, Node head2) {
+        Node margeLL = new Node(-1);
+        Node temp = margeLL;
+
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+        while (head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while (head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+
+        return margeLL.next;
+    }
+
+    public Node margeSort(Node head) {
+        //* base case
+        if (head == null || head.next == null) {
+            return head;
+        }
+        //* find mid
+        Node mid = getMid();
+
+        //* left & right Marge sort
+        Node rightHalf = mid.next;
+        mid.next = null;
+        Node newLeft = margeSort(head);
+        Node newRight = margeSort(rightHalf);
+
+        //* marge
+        return marge(newLeft, newRight);
+    }
+
+    public void Print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        MargeSort ll = new MargeSort();
+        ll.addLast(5);
+        ll.addLast(4);
+        ll.addLast(3);
+        ll.addLast(2);
+        ll.addLast(1);
+        ll.Print();
+        // ll.head = margeSort(ll.head);
+        ll.Print();
+    }
+}
