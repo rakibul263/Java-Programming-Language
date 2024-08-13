74343f29c69c6e6ec0acfcca2e953a1d26100584
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-08-13 16:47:30 +0600

    doubly linked list

---
+++ Linked LIst/Doubly_Linked_List.java
@@ -0,0 +1,88 @@
+public class Doubly_Linked_List {
+    public class Node {
+        int data;
+        Node prev;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    // ! add Method
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+    }
+
+    public void addAnyPosition(int index, int data) {
+        if (index == 0) {
+            addFirst(data);
+            return;
+        } else if (index > size + 1) {
+            System.out.println("Please! enter a valid index");
+            return;
+        } else if (index == size + 1) {
+            addLast(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+        while (i < index - 1) {
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next.prev = newNode;
+        temp.next = newNode;
+        newNode.prev = temp;
+    }
+
+    // ! print method
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Doubly_Linked_List ll = new Doubly_Linked_List();
+        ll.addFirst(3);
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.addAnyPosition(6, 100);
+        ll.print();
+        System.out.println("Size: " + ll.size);
+    }
+}
