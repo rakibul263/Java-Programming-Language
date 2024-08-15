4ed60d0f8d6655eb1ff7ed1f4a52814630cfaa89
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-08-15 12:40:06 +0600

    problem solve

---
+++ Problem Solve/Linear_search.java
@@ -0,0 +1,14 @@
+// !
+// https://www.naukri.com/code360/problems/linear-search_624470?utm_source=youtube&utm_medium=affiliate&utm_campaign=parikh_youtube
+public class Linear_search {
+
+    public static int linearSearch(int arr[], int x) {
+        // Your code goes here
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
