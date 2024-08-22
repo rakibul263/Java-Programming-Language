b2ed73f8c74b11b75ce1d459259d004f84418345
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-08-23 01:29:00 +0600

    Height of the tree

---
+++ Binary Tree/Height_of_the_tree.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Height_of_the_tree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static class BinaryTree {
+        int index = -1;
+
+        public Node buildTree(int nodes[]) {
+            index++;
+            if (nodes[index] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[index]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+
+        public int heightNode(Node root) {
+            if (root == null) {
+                return 0;
+            }
+            int lh = heightNode(root.left);
+            int rh = heightNode(root.right);
+            return Math.max(lh, rh) + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree bt = new BinaryTree();
+        Node root = bt.buildTree(nodes);
+        System.out.println(bt.heightNode(root));
+    }
+}
