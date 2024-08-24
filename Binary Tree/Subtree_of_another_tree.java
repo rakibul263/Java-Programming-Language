2c5537203049116d8a7b7ec26fb12f5bad064411
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-08-24 23:55:56 +0600

    Diameter

---
+++ Binary Tree/Subtree_of_another_tree.java
@@ -0,0 +1,21 @@
+public class Subtree_of_another_tree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.right = new Node(6);
+    }
+}
