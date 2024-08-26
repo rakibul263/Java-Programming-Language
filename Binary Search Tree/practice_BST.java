2be597145d12804d261d091d5dd59420753400cf
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-08-26 12:04:19 +0600

    BST build

---
+++ Binary Search Tree/practice_BST.java
@@ -0,0 +1,46 @@
+public class practice_BST {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+        if (root.data > val) {
+            root.left = insert(root.left, val);
+        }
+        if (root.data < val) {
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 6, 4, 5, 3, 7, 8, 9, 10 };
+        Node root = null;
+
+        for (int i = 0; i < nodes.length; i++) {
+            root = insert(root, nodes[i]);
+        }
+        inorder(root);
+        System.out.println();
+    }
+}
