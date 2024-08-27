c742d01c5c0955cfe3e7354b0f12bae610b077ba
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-08-28 00:22:54 +0600

    root to leaf path

--- Binary Search Tree/root_to_leaf_path.java
+++ Binary Search Tree/root_to_leaf_path.java
@@ -1,7 +1,73 @@
 import java.util.*;
 
 public class root_to_leaf_path {
-    public static void main(String[] args) {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
 
+        public Node(int data) {
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+        if (root.data < val) {
+            // right
+            root.right = insert(root.right, val);
+        } else if (root.data > val) {
+            root.left = insert(root.left, val);
+        }
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+    // !---------------Start print root to Left---------------------------
+
+    public static void printPath(ArrayList<Integer> path) {
+        for (int i = 0; i < path.size(); i++) {
+            System.out.print(path.get(i) + "->");
+        }
+        System.out.println("NULL");
+    }
+
+    public static void printRootToLeaf(Node root, ArrayList<Integer> path) {
+        if (root == null) {
+            return;
+        }
+
+        path.add(root.data);
+        if (root.left == null && root.right == null) {
+            printPath(path);
+        }
+        printRootToLeaf(root.left, path);
+        printRootToLeaf(root.right, path);
+        path.remove(path.size() - 1);
+    }
+    // !--------------End print root to Left------------------------------
+
+    public static void main(String[] args) {
+        int values[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
+        Node root = null;
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+
+        inorder(root);
+        System.out.println();
+
+        printRootToLeaf(root, new ArrayList<>());
     }
 }
