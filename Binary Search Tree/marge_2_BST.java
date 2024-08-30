15e411bca7cc1a2edae5ae4661937a5eb72c933c
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-08-30 10:30:21 +0600

    create a node

---
+++ Binary Search Tree/marge_2_BST.java
@@ -0,0 +1,16 @@
+public class marge_2_BST {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
