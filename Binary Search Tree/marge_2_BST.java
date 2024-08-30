2f4852a75c4fafbc6d30382436c7aba43b8fdda9
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-08-30 10:53:58 +0600

    marge two bst

--- Binary Search Tree/marge_2_BST.java
+++ Binary Search Tree/marge_2_BST.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class marge_2_BST {
     static class Node {
         int data;
@@ -10,6 +12,65 @@ public class marge_2_BST {
         }
     }
 
+    public static void getInorder(Node root, ArrayList<Integer> arr) {
+        if (root == null) {
+            return;
+        }
+        getInorder(root.left, arr);
+        arr.add(root.data);
+        getInorder(root.right, arr);
+    }
+
+    public static Node createBST(ArrayList<Integer> array, int st, int end) {
+        if (st > end) {
+            return null;
+        }
+        int mid = (st + end) / 2;
+        Node root = new Node(array.get(mid));
+        root.left = createBST(array, st, mid - 1);
+        root.right = createBST(array, mid + 1, end);
+        return root;
+    }
+
+    public static Node margeTwoBST(Node root1, Node root2) {
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        getInorder(root1, arr1);
+
+        ArrayList<Integer> arr2 = new ArrayList<>();
+        getInorder(root2, arr2);
+
+        ArrayList<Integer> finalArrayList = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < arr1.size() && j < arr2.size()) {
+            if (arr1.get(i) <= arr2.get(j)) {
+                finalArrayList.add(arr1.get(i));
+                i++;
+            } else {
+                finalArrayList.add(arr2.get(j));
+                j++;
+            }
+        }
+        while (i < arr1.size()) {
+            finalArrayList.add(arr1.get(i));
+            i++;
+        }
+        while (j < arr2.size()) {
+            finalArrayList.add(arr2.get(j));
+            j++;
+        }
+
+        return createBST(finalArrayList, 0, finalArrayList.size() - 1);
+    }
+
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
     public static void main(String[] args) {
         Node root1 = new Node(2);
         root1.left = new Node(1);
@@ -18,5 +79,9 @@ public class marge_2_BST {
         Node root2 = new Node(9);
         root2.left = new Node(3);
         root2.right = new Node(12);
+
+        Node root = margeTwoBST(root1, root2);
+        preorder(root);
+        System.out.println();
     }
 }
