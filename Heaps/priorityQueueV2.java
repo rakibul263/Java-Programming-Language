edad02ad6b00ec765dd60b9a676efdd4d79c87b7
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-09-05 20:23:27 +0600

    priority queue complexity added

--- Heaps/priorityQueueV2.java
+++ Heaps/priorityQueueV2.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class priorityQueueV2 {
     public static void main(String[] args) {
         PriorityQueue<Integer> pq = new PriorityQueue();
-        pq.add(4);
+        pq.add(4); // O(logN)
         pq.add(5);
         pq.add(1);
         while (!pq.isEmpty()) {
-            System.out.print(pq.peek() + " ");
-            pq.remove();
+            System.out.print(pq.peek() + " ");// O(1)
+            pq.remove();// O(logN)
         }
         System.out.println();
     }
