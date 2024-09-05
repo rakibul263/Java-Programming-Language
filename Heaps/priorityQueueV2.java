c972c1b57c6b8c7b969a88fb1f3d59685d48e5f7
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-09-05 20:22:13 +0600

    priority queue

---
+++ Heaps/priorityQueueV2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class priorityQueueV2 {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue();
+        pq.add(4);
+        pq.add(5);
+        pq.add(1);
+        while (!pq.isEmpty()) {
+            System.out.print(pq.peek() + " ");
+            pq.remove();
+        }
+        System.out.println();
+    }
+}
