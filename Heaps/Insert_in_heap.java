fd0e15a85d75510998cd601152573fdf3bfdc7fa
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-09-08 20:03:45 +0600

    Delete value

--- Heaps/Insert_in_heap.java
+++ Heaps/Insert_in_heap.java
@@ -20,6 +20,43 @@ public class Insert_in_heap {
         public int peek(){
             return arr.get(0);
         }
+
+        private void heapfify(int i){
+            int left = 2*i+1;
+            int right = 2*i+2;
+            int minIndex = i;
+
+            if(left < arr.size() && arr.get(left) < arr.get(minIndex)){
+                minIndex = left;
+            }
+            if(right < arr.size() && arr.get(right) < arr.get(minIndex)){
+                minIndex = right;
+            }
+
+            if(minIndex != i){
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIndex));
+                arr.set(minIndex, temp);
+
+                heapfify(minIndex);
+            }
+
+        }
+        public int remove(){
+            int data = arr.get(0);
+
+            //step-01 1st and last node swap
+            int temp = arr.get(0);
+            arr.set(0, arr.get(arr.size()-1));
+            arr.set(arr.size()-1, temp);
+
+            //delete last node
+            arr.remove(arr.size()-1);
+
+            //heapfify
+            heapfify(0);
+            return data;
+        }
     }
     public static void main(String[] args) {
 
