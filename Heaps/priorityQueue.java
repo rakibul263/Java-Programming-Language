07f9ce8b8e377c3bf8770d241e932ca4bff5d4e1
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-09-08 20:11:05 +0600

    updated

--- Heaps/priorityQueue.java
+++ Heaps/priorityQueue.java
@@ -1,15 +1,15 @@
-import java.util.*;
+import java.util.PriorityQueue;
 
 public class priorityQueue {
     public static void main(String[] args) {
-        PriorityQueue<Integer>pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         pq.add(5);
         pq.add(2);
         pq.add(3);
         pq.add(8);
 
-        while(!pq.isEmpty()) {
+        while (!pq.isEmpty()) {
             System.out.println(pq.peek());
             pq.remove();
         }
